a551910cfc06eeccc613aa55e5914e49f64475fd
Unknown <luke123t@gmail.com>
2018-06-08 19:53:31 -0300

    Add Basic Level Generation Method

---
+++ Q_Bert3D/Assets/Editor/Tests/Services/LevelGeneration/Tests_LevelGenerator.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using FireBullet.QBert.Services;
+using System;
+
+namespace Editor
+{   
+    public class Tests_LevelGenerator 
+    {
+        [TestCase(0)]
+        [TestCase(15)]
+        [TestCase(-1)]
+        [TestCase(2)]
+        [TestCase(5)]
+        [TestCase(-10)]
+        [TestCase(3)]
+        [TestCase(8)]
+        [TestCase(-4)]
+        [TestCase(-1000)]
+        [TestCase(1000)]
+        public void GenerateMap_InputRowParameter_MustBeGreaterThan0(int numberOfRows)
+        {
+            LevelGenerator generator = new LevelGenerator();
+
+            if(numberOfRows <= 0)
+            {
+                Assert.That(() => generator.GenerateLevel(numberOfRows),
+                            Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+			}
+        }
+
+        [TestCase(1, 6)]
+        [TestCase(2, 12)]
+        [TestCase(3, 18)]
+        [TestCase(4, 24)]
+        public void GenerateMap_CorrectNumberOfHexesCreated_MustBeCorrect(int numberOfRows, int expectedNumberOfHexes)
+        {
+            LevelGenerator generator = new LevelGenerator();
+
+            generator.GenerateLevel(numberOfRows);
+
+            Assert.AreEqual(generator.Hexes.Count, expectedNumberOfHexes);
+        }
+    }
+}
---
+++ Q_Bert3D/Assets/Scripts/Services/LevelGeneration/LevelGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace FireBullet.QBert.Services
+{
+    /// <summary>
+    /// The Level Generator is an implementation of the ILevelGenerator
+    /// service which generates levels for the game.
+    /// </summary>
+    public class LevelGenerator : MonoBehaviour
+    {
+        #region Public Variables
+        public List<GameObject> Hexes { get { return m_hexes; } }
+        #endregion
+
+        #region Private Variables
+        private List<GameObject> m_hexes = new List<GameObject>();
+        #endregion
+
+        #region Main Methods
+        public void GenerateLevel(int numberOfRows)
+        {
+            if (numberOfRows <= 0)
+                throw new System.ArgumentOutOfRangeException("Number of Rows must be greater than 0");
+
+            CreateLevel(numberOfRows);
+        }
+        #endregion
+
+        #region Utility Methods
+        private void CreateLevel(int numberOfRows)
+        {
+            int numberOfHexesToCreate = numberOfRows * 6;
+            for (int i = 0; i < numberOfHexesToCreate; i++)
+            {
+                m_hexes.Add(null);
+            }
+        }
+        #endregion
+    }
+}
