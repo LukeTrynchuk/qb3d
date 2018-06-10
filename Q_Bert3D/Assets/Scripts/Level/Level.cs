ab817a19c512effaeca17be7ec6d4757efc1c2c6
Unknown <luke123t@gmail.com>
2018-06-10 14:18:09 -0300

    Add Level Scriptable Object

---
+++ Q_Bert3D/Assets/Scripts/Level/Level.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace FireBullet.QBert.Levels
+{
+    /// <summary>
+    /// The Level scriptable asset will 
+    /// contain the information about each 
+    /// level.
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewLevel", menuName = "Levels/Level")]
+    public class Level : ScriptableObject
+    {
+        public int NumberOfRows;
+    }
+}
