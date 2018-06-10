9e59caba59ae651e54fe4d72f762b284c311bbfe
Unknown <luke123t@gmail.com>
2018-06-10 14:30:36 -0300

    Add Level Containers

---
+++ Q_Bert3D/Assets/Scripts/Level/GameLevelData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace FireBullet.QBert.Levels
+{
+    /// <summary>
+    /// Game Level data is a container for all the 
+    /// world containers. The game level data is 
+    /// the object that will be serialized and
+    /// put on the server.
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewGameLevelData", menuName = "Levels/GameLevelDataContainer")]
+    public class GameLevelData : ScriptableObject 
+    {
+        public WorldContainer[] Worlds;
+    }
+}
---
+++ Q_Bert3D/Assets/Scripts/Level/WorldContainer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FireBullet.QBert.Levels
+{
+    /// <summary>
+    /// The World Container contains a series of
+    /// levels which make up a world / section.
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewWorldContainer", menuName = "Levels/World")]
+    public class WorldContainer : ScriptableObject
+    {
+        public Level[] Levels;
+    }
+}
