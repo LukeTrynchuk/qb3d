ed2b63a15a5c8195e2546fdd19aae52a63627190
Unknown <luke123t@gmail.com>
2018-06-10 00:07:10 -0300

    Add Build Version UI Text

---
+++ Q_Bert3D/Assets/Scripts/UI/SetBuildVersionText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using FireBullet.QBert.Utility;
+using UnityEngine.UI;
+
+namespace FireBullet.QBert.UI
+{
+    /// <summary>
+    /// The Set build version text will set a 
+    /// text object with the build version
+    /// information.
+    /// </summary>
+    public class SetBuildVersionText : MonoBehaviour
+    {
+        #region Private Variables
+        [SerializeField]
+        private Text m_text;
+        #endregion
+
+        #region Main Methods
+        private void Start() => m_text.text = $"BUILD {ApplicationUtility.GetBuildVersion()}.{ApplicationUtility.GetBuildNumber()} ";
+		#endregion
+	}
+}
---
+++ Q_Bert3D/Assets/Scripts/Utility/ApplicationUtility.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace FireBullet.QBert.Utility
+{
+    /// <summary>
+    /// The Application Utility class is responsible
+    /// for containing a series of helper methods
+    /// for other objects to gain information about 
+    /// the application.
+    /// </summary>
+    public class ApplicationUtility
+    {
+        #region Main Methods
+        public static string GetBuildVersion() => Application.version;
+        public static string GetBuildNumber() => UnityEditor.PlayerSettings.iOS.buildNumber;
+        #endregion
+    }
+}
